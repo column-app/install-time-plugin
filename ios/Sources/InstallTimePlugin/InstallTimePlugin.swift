a304e4d6cae7379adfb76baef427a2dbed177a09
Andrea La Grotteria <lagrotteria.andrea@gmail.com>
2025-06-20 15:53:53 +0200

    fix for ios

--- ios/Sources/InstallTimePlugin/InstallTimePlugin.swift
+++ ios/Sources/InstallTimePlugin/InstallTimePlugin.swift
@@ -2,7 +2,13 @@ import Foundation
 import Capacitor
 
 @objc(InstallTimePlugin)
-public class InstallTimePlugin: CAPPlugin {
+public class InstallTimePlugin: CAPPlugin, CAPBridgedPlugin {
+    public let identifier = "InstallTimePlugin"
+    public let jsName = "InstallTime"
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "getInstallTime", returnType: CAPPluginReturnPromise)
+    ]
+
     @objc func getInstallTime(_ call: CAPPluginCall) {
         if let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last,
            let attrs = try? FileManager.default.attributesOfItem(atPath: documentsUrl.path),
